887f9e4a694276fad155c36f2b03cf2ce6343654
g0imaX <83723952+g0imaX@users.noreply.github.com>
2024-05-03 16:37:28 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_apk_data():
+    url = "https://apksos.com/"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    app_titles = soup.select("div.col-md-12.col-sm-9.vcenter.apptitle")
+    app_icons = soup.select("div.col-md-12.col-sm-3.vcenter")
+
+    apk_data = []
+    for app_title, app_icon in zip(app_titles, app_icons):
+        apk_name_element = app_title.find("p")
+        apk_img_element = app_icon.find("img")
+        if apk_name_element and apk_img_element:
+            apk_name = apk_name_element.get_text(strip=True)
+            apk_img_src = apk_img_element.get("data-original")
+            apk_info = {
+                "name": apk_name,
+                "image_src": apk_img_src
+            }
+            apk_data.append(apk_info)
+
+    return jsonify(apk_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
