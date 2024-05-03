8922fb6e1d9fd285bfb0374bda9241b1cc523a93
g0imaX <83723952+g0imaX@users.noreply.github.com>
2024-05-03 16:46:39 +0600

    Update download.py

--- download.py
+++ download.py
@@ -1,32 +1,61 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-@app.route('/app', methods=['GET'])
-def get_apk_data():
-    url = "https://apksos.com/"
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "html.parser")
+@app.route('/app-info/<path:app_id>')
+def get_app_info(app_id):
+    try:
+        # Construct the URL
+        url = f"https://www.appbrain.com/app/{app_id}"
+        # Requesting the page
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
 
-    app_titles = soup.select("div.col-md-9.col-sm-9.apptitle")
-    app_icons = soup.select("div.col-md-3.col-sm-3.vcenter")
+        # Getting Title
+        appTitle = soup.h1.string
 
-    apk_data = []
-    for app_title, app_icon in zip(app_titles, app_icons):
-        apk_name_element = app_title.find("p")
-        apk_img_element = app_icon.find("img")
-        if apk_name_element and apk_img_element:
-            apk_name = apk_name_element.get_text(strip=True)
-            apk_img_src = apk_img_element.get("data-original")
-            apk_info = {
-                "name": apk_name,
-                "image_src": apk_img_src
-            }
-            apk_data.append(apk_info)
+        # Getting Second Title
+        appSecTitle = soup.select_one("h2.app-short-description").get_text(strip=True)
 
-    return jsonify(apk_data)
+        # Getting AppIcon
+        appIcon = soup.select_one("div.img-wrapper").find("img")["src"]
+
+        # Getting About App
+        aboutApp = [element.get_text(strip=True) for element in soup.select("div.col-12.col-sm-6")]
+
+        # Getting App Description
+        appDescription = soup.select_one("a.link")["data-contents"].replace('<br>', ' ')
+
+        # Getting List Of Screenshot
+        appScreenshot = [img["src"] for img in soup.select("div.swiper-wrapper img")]
+
+        # Getting Table
+        appTable = soup.select_one("table.table.table-striped")
+
+        # Extract data from the table
+        data = {}
+        for row in appTable.find_all("tr"):
+            key = row.find("td").text.strip()
+            value = row.find("td").find_next_sibling().text.strip()
+            data[key] = value
+
+        # Creating response
+        response = {
+            "app_title": appTitle,
+            "second_title": appSecTitle,
+            "app_icon": appIcon,
+            "about_app": aboutApp,
+            "app_description": appDescription,
+            "app_screenshot": appScreenshot,
+            "app_data": data
+        }
+
+        return jsonify(response), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
